7f28f00743ff9b416970f9c3dc4ff701f97dfd15
isisgoncalves <isisvow@gmail.com>
2023-05-04 10:26:37 -0300

    Comentar
    Como ignorar/comentar códigos

---
+++ comentarios.js
@@ -0,0 +1,15 @@
+/*
+F12: Dev tools
+scripts.js (Java Script)
+<script src= " " > </script>
+  ./ (Diretório Local)
+    ./script.js
+No script.js: Ex.:
+  console.log('Bem vindos ao Starter!!!')
+    Atualizar no Dev tools da página, Aba Console, para visualização de mensagem.
+
+Como ignorar/comentar códigos:
+  Acrescentar // antes do código (para 1 linha).
+Para mais de 1 linha (bloco):
+  Antes do código adicionar /*, e no fim do código */
+
