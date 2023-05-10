00b66a7bc5192381520c307df6d6add5d5cd29fd
isisgoncalves <isisvow@gmail.com>
2023-05-10 08:49:18 -0300

    Agrupando declarações

--- comentarios.js
+++ comentarios.js
@@ -33,3 +33,10 @@ Para declarar uma variável podemos usar var, let e const.
 Para atribuírmos valores à essa variável utilizamos o caractere =.
 E para vermos o tipo dessa variável podemos utilizar a função console.log(typeof variavel).
 */
+
+/*
+Agrupando declarações
+É possível agrupar diversas declarações de variáveis em JavaScript usando let, const ou var e colocando uma vírgula entre os nomes das variáveis a serem criadas, por exemplo: let name, age, isStudent. 
+Para mostrar mais de uma variável no console.log também é possível usando vírgula, por exemplo:
+console.log(name, age, isStudent).
+*/
