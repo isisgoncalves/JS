e8b5d6d9a288d2bcfcb2ef0b5c6add261a6bc41f
isisgoncalves <isisvow@gmail.com>
2023-06-01 18:00:51 -0300

    Exercícios

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-console.log('Bem vindos ao starter!!!')
+//console.log('Bem vindos ao starter!!!')
 
+let animals = ['gato', 'rato', 'pato']
+
+animals.push('cavalo')
+
+animals.unshift('macaco')
+
+console.log(animals)
