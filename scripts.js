f98a0ad91f6bd60a44b0fa737c031fb656a63715
isisgoncalves <isisvow@gmail.com>
2023-06-03 17:58:17 -0300

    Estudando

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 //console.log('Bem vindos ao starter!!!')
 
-let animals = ['gato', 'rato', 'pato']
+// let animals = ['gato', 'rato', 'pato']
 
-animals.push('cavalo')
+// animals.push('cavalo')
 
-animals.unshift('macaco')
+// animals.unshift('macaco')
 
-console.log(animals)
+// console.log(animals)
+
+//console.log("10" + 5)
+
+// console.log (25 * 'casa')
+
+let numberInfinity = (42/0)
+console.log (numberInfinity)
