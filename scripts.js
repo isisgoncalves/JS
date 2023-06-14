234d310fa4e778b97baacdd392cbe473f0feb833
isisgoncalves <isisvow@gmail.com>
2023-06-14 20:06:23 -0300

    Praticando

--- scripts.js
+++ scripts.js
@@ -1,27 +1,4 @@
-//console.log('Bem vindos ao starter!!!')
-
-// let animals = ['gato', 'rato', 'pato']
-
-// animals.push('cavalo')
-
-// animals.unshift('macaco')
-
-// console.log(animals)
-
-//console.log("10" + 5)
-
-// console.log (25 * 'casa')
-
-// let numberInfinity = (42/0)
-// console.log (numberInfinity)
-
-// const person = {
-//   name: 'Isis',
-//   age: 35,
-// }
-// delete person.age
-// console.log(person)
-
-let age = 19
-const Drive = age >= 18 ? 'can drive' : "can't drive"
-console.log(Drive)
+let temperature = 36
+if (temperature >= 37){
+  console.log('febre')
+}
