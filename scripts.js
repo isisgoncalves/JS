a668782174dae0231f6095177e7d809a7fb47c25
isisgoncalves <isisvow@gmail.com>
2023-06-19 15:49:24 -0300

    Operações matemáticas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-// let person = prompt("What's your name?")
-// alert("Hello " + person)
+/*
+  Capturar 2 números
+  e fazer as operações matemáticas
+  de soma, subtração, multiplicação,
+  divisão e resto da divisão.
 
+  E para cada operação, mostrar um alerta
+  com o resultado.
+*/
+let numberOne = prompt('Digite um número')
+let numberTwo = prompt('Digite mais um número')
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+const soma = numberOne + numberTwo
+const sub = numberOne - numberTwo
+const multi = numberOne * numberTwo
+const div = numberOne / numberTwo
+const resto = numberOne % numberTwo
+alert('Soma= ' + soma)
+alert('Subtração= ' + sub)
+alert('Multiplicação= ' + multi)
+alert('Divisão= ' + div)
+alert('Resto da divisão= ' + resto)
